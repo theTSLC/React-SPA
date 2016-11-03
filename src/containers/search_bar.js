943fe1961b509d0f902d6e2194db870b34242470
Tom LeConey <tleconey@gmail.com>
2016-11-03 15:37:38 -0500

    intial test succesful

---
+++ src/containers/search_bar.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+export default class SearchBar extends Component {
+	constructor(props) {
+		super(props) 
+
+		this.state = {};
+	}
+
+  render() {
+  	return (
+  		<form className="input-group">
+  		  <input />
+  		</form>
+  	);
+  }
+}
