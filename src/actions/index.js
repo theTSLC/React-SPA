037ddd24060454a0197c39155da4c1437bdf68d7
Tom LeConey <tleconey@gmail.com>
2016-11-15 15:45:58 -0600

    added axios and created ajax request for api call

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,19 @@
+import axios from 'axios';
+
 const API_KEY = 'c5b4b6f9569a2e326f6c110bcede574a';
+//using ES6 template strings
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+//useful to keep our action types similiar between action creators and reducers
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+	const url = `${ROOT_URL}&q=${city},us`;
+  const request = avios.get(url);
+
+	return {
+      type: FETCH_WEATHER,
+      //payload are optional property that goes along with actions that contains additional data that describes this particular action
+      payload: request
+		};
+}
