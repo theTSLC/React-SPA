3ef0d8ba954a336ef424be77eea985e857b59171
Tom LeConey <tleconey@gmail.com>
2016-11-16 14:49:45 -0600

    finished most of Redux state manegment

--- src/actions/index.js
+++ src/actions/index.js
@@ -11,12 +11,11 @@ export function fetchWeather(city) {
 	const url = `${ROOT_URL}&q=${city},us`;
   const request = axios.get(url);
 
-  console.log('Request:', request);
 
 	return {
       type: FETCH_WEATHER,
       //payload are optional property that goes along with actions that contains additional data that describes this particular action
-      //returning the request, the promise, on the payload here
+      //returning the request, the promise, as the payload
       payload: request
 		};
 }
--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,7 +1,12 @@
-// for reducers always have first argument of state we're exporting and second argument as action
-export default function(state = null, action) {
-	console.log('Action received', action);
+import { FETCH_WEATHER } from '../actions/index';
 
-	
-	return state;
+// for reducers always have first argument of state we're exporting and second argument as action
+export default function(state = [], action) {
+  switch (action.type) {
+    case FETCH_WEATHER: 
+      // making sure to return new instance of state, not mutate state as if with state.push
+      // return state.concat([action.payload.data]); ES6 equivalent syntax below
+      return [action.payload.data, ...state]; // [city, city, city] NOT [city, [city, city]]
+  }
+  return state;
 }
